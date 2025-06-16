11f65bfb249f4da3074ef38cd9c038e932e78512
LuisFernandoBS <luisfernando142536@gmail.com>
2025-06-16 19:43:50 -0300

    Criando componente para visualização do card na tela de historico

--- src/app/historico/page.tsx
+++ src/app/historico/page.tsx
@@ -2,12 +2,14 @@
 import { useEffect, useState } from 'react';
 import Image from "next/image";
 import Header from "@/components/header";
+import ModalImagemCard from "@/components/modalImagemCard";
 import { useHistorico } from '@/hooks/useHistorico';
 
 
 export default function Historico() {
     const { historico } = useHistorico();
     const [listaHistorico, setListaHistorico] = useState<any[]>([]);
+    const [urlImagemCard, setUrlImagemCard] = useState<string>("");
 
     useEffect(() => {
         if(historico.length !== 0) {
@@ -15,8 +17,14 @@ export default function Historico() {
         }
     }, [historico]);
 
+    const abrirModalImagemCard = (urlImagem: string) => {
+        setUrlImagemCard(urlImagem);
+    };
+
     return (
         <div className="items-center justify-center h-screen text-center">
+            {urlImagemCard && <ModalImagemCard urlImagem={urlImagemCard} fecharImagem={setUrlImagemCard} />
+            }
             <div className="py-5 h-auto max-h-[120px]">
                 <Header page="historico" />
             </div>
@@ -31,14 +39,15 @@ export default function Historico() {
                             {listaHistorico.map((duelo, index) => (
                                 <div key={index} className="col-span-1 flex justify-center items-center p-3 bg-gray-800 rounded-lg shadow-md my-1">
                                     <div className='grid w-full grid-cols-6 grid-rows-1 gap-1'>
-                                        <div className="col-span-2 flex justify-center items-center p-4 bg-gray-900 rounded-lg ">
+                                        <div className="col-span-2 flex justify-center items-center p-4 bg-gray-900 border border-red-500 rounded-lg ">
                                             <h2 className='text-[15px] font-medium text-white flex items-center'>
                                                 <Image
                                                     src={`${duelo.card1.img}/high.webp`}
                                                     alt="card1"
                                                     width={50}
                                                     height={80}
-                                                    className="object-cover mr-3"
+                                                    className="object-cover mr-3 cursor-pointer"
+                                                    onClick={() => abrirModalImagemCard(duelo.card1.img)}
                                                 />
                                                 <span className="mr-2">
                                                     {duelo.card1.nome} 
@@ -52,23 +61,24 @@ export default function Historico() {
                                                     alt="card1"
                                                     width={50}
                                                     height={80}
-                                                    className="object-cover ml-3"
+                                                    className="object-cover ml-3 cursor-pointer"
+                                                    onClick={() => abrirModalImagemCard(duelo.card2.img)}
                                                 />
                                             </h2>
                                         </div>
-                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 rounded-lg ">
+                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 border border-amber-500 rounded-lg ">
                                             <h2 className='text-[15px] font-medium text-white text-center w-full'>HP</h2>
                                             <h2 className='text-[15px] font-medium text-white'>{duelo.card1.hp} X {duelo.card2.hp}</h2>
                                         </div>
-                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 rounded-lg ">
+                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 border border-amber-500 rounded-lg ">
                                             <h2 className='text-[15px] font-medium text-white text-center w-full'>Ataque</h2>
                                             <h2 className='text-[15px] font-medium text-white'>{duelo.card1.ataque} X {duelo.card2.ataque}</h2>
                                         </div>
-                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 rounded-lg ">
+                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 border border-amber-500 rounded-lg ">
                                             <h2 className='text-[15px] font-medium text-white text-center w-full'>Tipo</h2>
                                             <h2 className='text-[12px] font-medium text-white'>{duelo.card1.tipo} X {duelo.card2.tipo}</h2>
                                         </div>
-                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 rounded-lg ">
+                                        <div className="col-span-1 flex flex-col h-full justify-center p-4 bg-gray-900 border border-amber-500 rounded-lg ">
                                             <h2 className='text-[15px] font-medium text-white text-center w-full'>Raridade</h2>
                                             <h2 className='text-[12px] font-medium text-white'>{duelo.card1.raridade} X {duelo.card2.raridade}</h2>
                                         </div>
---
+++ src/components/modalImagemCard.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+interface Props {
+    urlImagem:string
+    fecharImagem:(url:string) => void;
+}
+
+export default function ModalImagemCard({urlImagem,fecharImagem}:Props) {
+    return(
+        <div
+        className="fixed inset-0 z-50 grid place-content-center bg-black/90 p-4"
+        role="dialog"
+        id="modalImagemCard"
+        aria-modal="true"
+        aria-labelledby="ModalImagemCard"
+        >
+            <div className="flex flex-col w-full max-w-md min-w-[440px] h-[700px] justify-center text-center items-center rounded-lg p-6 shadow-lg">
+                <div className="w-full">
+                    <button
+                            type="button"
+                            className="-me-4 -mt-4 cursor-pointer rounded-full p-2 transition-colors bg-gray-500 mb-6 focus:outline-none text-gray-300 hover:bg-gray-800 hover:text-gray-300"
+                            aria-label="Close"
+                            onClick={()=>{fecharImagem("")}}
+                        >
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="size-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                        />
+                        </svg>
+                    </button>
+                </div>
+                <Image
+                src={`${urlImagem}/high.webp`}
+                alt="card"
+                width={420}
+                height={600}
+                loading="eager"
+                className={`object-cover rounded-2xl transition hover:scale-[105%]`}
+                />
+            </div>
+        </div>
+    )
+}
